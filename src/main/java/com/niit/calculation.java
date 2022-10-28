2a465bc289650c71313598ab8b5eb2389afa540a
rishavemail786 <rishavemail786@gmail.com>
2022-10-28 13:48:54 +0530

    completed

--- src/main/java/com/niit/calculation.java
+++ src/main/java/com/niit/calculation.java
@@ -1,2 +1,24 @@
-package com.niit;public class calculation {
+package com.niit;
+
+public class calculation {
+    int addition(int num1,int num2){
+        return num1+num2;
+
+    }
+    public int subtraction(int number1,int number2){
+        return number1 - number2;
+    }
+    public int multiplication(int number1,int number2){
+        if(number1 == 0 || number2 == 0){
+            throw new ArithmeticException();
+        }
+        return number1 * number2;
+    }
+    public int division(int number1,int number2){
+        if(number1 < number2){
+            throw new ArithmeticException();
+        }
+        return number1 / number2;
+    }
+
 }
--- src/test/java/com/niit/calculationTest.java
+++ src/test/java/com/niit/calculationTest.java
@@ -1,4 +1,39 @@
+package com.niit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class calculationTest {
-  
+    calculation calculation;
+    @BeforeEach
+    void setUp(){
+        calculation=new calculation();
+    }
+    @AfterEach
+    void tearDown(){
+        calculation=null;
+    }
+
+    @Test
+    void addition() {
+        assertEquals(10,calculation.addition(5,5));
+    }
+
+    @Test
+    void subtraction() {
+        assertEquals(2,calculation.subtraction(20,18));
+    }
+
+    @Test
+    void multiplication() {
+        assertEquals(100,calculation.multiplication(10,10));
+    }
+
+    @Test
+    void division() {
+        assertEquals(100,calculation.multiplication(10,10));
+    }
 }
